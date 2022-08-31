894a76c834b5ca36fecf7a868fa00af150c98067
CoderGirl <angi.adema@gmail.com>
2022-08-31 14:16:58 -0600

    add css & html.

---
+++ script.js
@@ -0,0 +1,13 @@
+//Button to start the game and thus a timer.
+
+//Timer started for the user to guess the word within 5 seconds of starting the time.
+var secondsLeft = 5;
+
+function setTime() {
+    var timeInterval = setInterval function () {
+        secondsLeft--;
+        
+    }
+}
+
+//Event listener to enter user key input.
